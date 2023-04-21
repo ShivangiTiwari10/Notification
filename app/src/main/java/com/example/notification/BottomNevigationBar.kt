577823a4c8c7bf82a3f335e1025f8e4f42e7c2b6
Shivangi <shivangit1111@gmail.com>
2023-04-21 19:28:31 +0530

    feat: create bottom bar

--- app/src/main/java/com/example/notification/BottomNevigationBar.kt
+++ app/src/main/java/com/example/notification/BottomNevigationBar.kt
@@ -2,10 +2,41 @@ package com.example.notification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.notification.databinding.ActivityBottomNevigationBarBinding
 
 class BottomNevigationBar : AppCompatActivity() {
+    private lateinit var binding: ActivityBottomNevigationBarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottom_nevigation_bar)
+        binding = ActivityBottomNevigationBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadFragment(HomeFragment())
+        binding.bottomNav.setOnItemSelectedListener() {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(MessageFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SettingFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
