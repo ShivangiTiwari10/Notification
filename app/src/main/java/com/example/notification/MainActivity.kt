8e61061ebcd8f101074f4ba93bcea511aaf181ab
Shivangi <shivangit1111@gmail.com>
2023-04-21 10:40:03 +0530

    feat: create NotificationChannel

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -3,46 +3,92 @@ package com.example.notification
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.example.notification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    @RequiresApi(Build.VERSION_CODES.O)
+
+    val channelId = "my_channel_id"
+    val channelName = "My Channel"
+    val notificationId = 0
+
     @SuppressLint("ServiceCast")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val channelId = "my_channel_id"
-        val channelName = "My Channel"
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(channelId, channelName, importance)
 
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-
-        val builder = NotificationCompat.Builder(this,channelId )
-            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
-            .setContentTitle("textTitle")
-            .setContentText("textContent")
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_baseline_insert_emoticon_24)
+            .setContentTitle("30 days of app development tutorial")
+            .setContentText("congratulation fo showing up today")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setColor(ContextCompat.getColor(this, R.color.purple_200))
             .setAutoCancel(true)
+            .build()
+        val notificationManager = NotificationManagerCompat.from(this)
+        binding.btn.setOnClickListener {
 
 
-        val notificationId = 1
+                notificationManager.notify(notificationId, notification)
 
-        notificationManager.notify(notificationId, builder.build())
+        }
+
+//        one way of notification
+//
+//        val importance = NotificationManager.IMPORTANCE_DEFAULT
+//        val channel = NotificationChannel(channelId, channelName, importance)
+//
+//        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        notificationManager.createNotificationChannel(channel)
+//
+//        val builder = NotificationCompat.Builder(this,channelId )
+//            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+//            .setContentTitle("textTitle")
+//            .setContentText("textContent")
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//            .setColor(ContextCompat.getColor(this, R.color.purple_200))
+//            .setAutoCancel(true)
+//
+//
+//        val notificationId = 1
+//
+//        notificationManager.notify(notificationId, builder.build())
     }
 
+
+//    Another way of notification
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "This is my notification channel"
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+
+    }
 
 }
